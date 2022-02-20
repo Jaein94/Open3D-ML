a066528aecdf73d854ef649eb849726e1b017d63
Jaein <jt1640@naver.com>
2022-02-20 16:56:30 +0900

    hesai refection 이슈 해결

--- test_main.py
+++ test_main.py
@@ -4,16 +4,17 @@ import open3d.ml.torch as ml3d
 from open3d.ml.vis import Visualizer, BoundingBox3D, LabelLUT
 from tqdm import tqdm
 import time
-
+import numpy as np
 
 cfg_file = "ml3d/configs/pointpillars_kitti.yml"
+# cfg_file = "ml3d/configs/pointpillars_kitti_40ch.yml"
 cfg = _ml3d.utils.Config.load_from_file(cfg_file)
 
 model = ml3d.models.PointPillars(**cfg.model)
-# cfg.dataset['dataset_path'] = "/home/amlab/KITTI_point"
+cfg.dataset['dataset_path'] = "/home/amlab/KITTI_point"
 # cfg.dataset['dataset_path'] = "/home/amlab/vtd_kitti_format"
 # cfg.dataset['dataset_path'] = "/home/amlab/os_test"
-cfg.dataset['dataset_path'] = "/home/amlab/hesai_test"
+# cfg.dataset['dataset_path'] = "/home/amlab/hesai_test"
 
 dataset = ml3d.datasets.KITTI(cfg.dataset.pop('dataset_path', None), **cfg.dataset)
 pipeline = ml3d.pipelines.ObjectDetection(model, dataset=dataset, device="gpu", **cfg.pipeline)
@@ -21,11 +22,11 @@ pipeline = ml3d.pipelines.ObjectDetection(model, dataset=dataset, device="gpu", 
 # download the weights.
 ckpt_folder = "./logs/"
 os.makedirs(ckpt_folder, exist_ok=True)
-ckpt_path = ckpt_folder + "pointpillars_kitti_202012221652utc.pth"
-pointpillar_url = "https://storage.googleapis.com/open3d-releases/model-zoo/pointpillars_kitti_202012221652utc.pth"
-if not os.path.exists(ckpt_path):
-    cmd = "wget {} -O {}".format(pointpillar_url, ckpt_path)
-    os.system(cmd)
+ckpt_path = ckpt_folder + "pointpillars_kitti_20220216.pth"
+# pointpillar_url = "https://storage.googleapis.com/open3d-releases/model-zoo/pointpillars_kitti_202012221652utc.pth"
+# if not os.path.exists(ckpt_path):
+#     cmd = "wget {} -O {}".format(pointpillar_url, ckpt_path)
+#     os.system(cmd)
 
 # load the parameters.
 pipeline.load_ckpt(ckpt_path=ckpt_path)
@@ -45,7 +46,10 @@ boxes = []
 data_list = []
 for idx in tqdm(range(10)):
     data = test_split.get_data(idx)
-    # print(data)
+    if np.max(np.unique(data['point'][:,3])) > 1:
+        data['point'][:,3] = np.round(data['point'][:,3]/255.0,3)
+    
+    print(np.unique(data['point'][:,3]))
     true_val = data['bounding_boxes']
     result = pipeline.run_inference(data)[0]
     pred = {
@@ -54,12 +58,12 @@ for idx in tqdm(range(10)):
     'bounding_boxes': result
     }
     data_list.append(pred)
-    # true = {
-    #     "name": 'KITTI_true' + '_' + str(idx),
-    #     'points': data['point'],
-    #     'bounding_boxes': true_val
-    # }
-    # data_list.append(true)
+    true = {
+        "name": 'KITTI_true' + '_' + str(idx),
+        'points': data['point'],
+        'bounding_boxes': true_val
+    }
+    data_list.append(true)
     
 
 vis.visualize(data_list, lut, bounding_boxes=None)
